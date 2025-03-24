007fe6c66c161b6973a68fa5a415f90571fd005a
rafelladicesaro <rdicesaro27@jcu.edu>
2025-03-24 03:46:02 +0000

    Added Lab1 console app

---
+++ Lab1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Step 1
+      Console.WriteLine("Rafaella Di Cesaro");
+      Console.WriteLine("IT-1050, Programming Logic");
+
+      // Step 2
+      /* Rafaella Di Cesaro
+      IT-1050, Programming Logic*/
+
+      //Step 3
+      int favNumber = 8;
+      string favLanguage = "SQL";
+      double numPrograms = 0;
+      bool progExperience = false; 
+
+      Console.WriteLine(favNumber);
+      Console.WriteLine(numPrograms);
+      Console.WriteLine(favLanguage);
+      Console.WriteLine(progExperience);
+
+      //Step 5 
+      const string mySchool = "John Carroll University";
+      Console.WriteLine(mySchool);
+
+    }
+  }
